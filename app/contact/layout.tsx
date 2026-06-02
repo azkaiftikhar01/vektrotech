a8ca4f75e17b15d2dc90e8dbfb41ba325439c5ba
azka <iftikharazka1@gmail.com>
2026-06-02 21:48:26 +0500

    changes 4

--- app/contact/layout.tsx
+++ app/contact/layout.tsx
@@ -1,7 +1,7 @@
 import { contactMetadata } from '@/lib/metadata'
 
 export const metadata = contactMetadata
-
+//
 export default function ContactLayout({ children }: { children: React.ReactNode }) {
   return children
 }
